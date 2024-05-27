a3fe2dc1c7dedec104f57e39aaa4c39c4aca74b8
Bryan Houlton <brhoulton@gmail.com>
2024-05-26 20:41:24 -0400

    fix(add): add

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,40 @@
-import dts from 'rollup-plugin-dts';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import dts from 'rollup-plugin-dts'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
-import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json')
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
+        format: 'cjs',
+        sourcemap: true
       },
       {
         file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
+        format: 'esm',
+        sourcemap: true
+      }
     ],
     plugins: [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      terser()
     ],
-    external: ["react", "react-dom"],
+    external: ['react', 'react-dom']
   },
   {
-    input: "src/index.ts",
-    output: [{ file: "dist/types.d.ts", format: "es" }],
-    plugins: [dts.default()],
-  },
-];
+    input: 'dist/cjs/index.d.ts',
+    output: [{ file: 'dist/types.d.ts', format: 'es' }],
+    plugins: [dts.default()]
+  }
+]
