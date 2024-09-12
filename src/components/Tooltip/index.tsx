681e8ddd76b441548d3c8b1c47ee93076eccf30b
Bryan Houlton <brhoulton@gmail.com>
2024-09-11 22:40:57 -0700

    fix(Tooltip): className

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,9 +1,9 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef } from 'react'
 
-import * as RTooltip from '@radix-ui/react-tooltip';
+import * as RTooltip from '@radix-ui/react-tooltip'
 
-import { cn } from '../../utilities';
-import { TooltipProps } from './Tooltip.types';
+import { cn } from '../../utilities'
+import { TooltipProps } from './Tooltip.types'
 
 export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
   (
@@ -14,6 +14,7 @@ export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
       styles,
       withArrow = true,
       disabled,
+      className,
       ...props
     },
     ref
@@ -33,7 +34,8 @@ export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
                 'data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade',
                 'data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade',
                 `z-50 w-[100%] select-none text-sm rounded-md bg-primary text-anti-primary`,
-                'px-2 py-1'
+                'px-2 py-1',
+                className
               )}
               sideOffset={5}
               ref={ref}
